dccbfc00313d9e33ca1780299c9e26561bdac7ef
Christopher <cjeong1021@gmail.com>
2023-06-12 17:46:43 -0400

    Revert "change coffee-journal image"
    This reverts commit f8c2ed6c07a4c190da4bf4e8750b2747c7eabde8.

--- src/components/Software/Software.js
+++ src/components/Software/Software.js
@@ -141,10 +141,8 @@ export default function Software() {
           </ul>
         </div>
         <div className='hidden md:block col-start-2 row-start-1 row-span-3 justify-self-center'>
-          <img
-            src={require('../../images/monsterhunter.png')}
-            className='h-64'
-          />
+          <img src={require('../../images/petstagram.jpeg')} className='h-64' />
+        </div>
       </div>
       <div className='my-6 md:grid md:grid-cols-2'>
         <div className='text-2xl decoration-sky-500 underline col-start-1'>
